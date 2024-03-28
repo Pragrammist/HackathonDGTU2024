1108ac3a7b7f014dc07fd9022763cad41b90bb18
MTR\kovalenko.v <kovalenko.v@medotrade.ru>
2024-03-28 18:16:26 +0300

    ДОбавил насследование для HistoryActionsContext

--- src/HackathonDGTU.Data/Contexts/HistoryActionsContext.cs
+++ src/HackathonDGTU.Data/Contexts/HistoryActionsContext.cs
@@ -1,24 +1,4 @@
-using HackathonDHTU.Data.Models;
-using Microsoft.EntityFrameworkCore;
-
-public class HistoryActionsContext : DbContext
+public class HistoryActionsContext : BaseAppContext
 {
-    protected DbSet<HistoryAction> HistoryActions { get; set; }
-
-    protected DbSet<HistoryAddAction> HistoryAddActions { get; set; }
-
-    protected DbSet<HistoryDeleteAction> HistoryDeleteActions { get; set; }
-
-    protected DbSet<HistoryRestoreAction> HistoryRestoreActions { get; set; }
-
-    protected DbSet<HistoryUpdateAction> HistoryUpdateActions { get; set; } 
-
     
-
-
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<HistoryAction>().UseTptMappingStrategy();
-    }
 }
